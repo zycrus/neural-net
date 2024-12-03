968ab5d2e785fb8a1b25e2fef79de02440eebcb0
zycrus <phantom.bot.202@gmail.com>
2024-12-03 13:40:52 +0800

    single headed attention

--- Makefile
+++ Makefile
@@ -2,18 +2,18 @@ APPNAME = Test
 EXT = .cpp
 OBJECTS = objects
 
-output: check-dir $(OBJECTS)\test.o
-	g++ -Wall $(OBJECTS)\test.o -o tests\builds\test.exe
+output: check-dir $(OBJECTS)/test.o
+	g++ -Wall $(OBJECTS)/test.o -o tests/builds/test.exe
 
-$(OBJECTS)\test.o: test.cpp
-	g++ -I../Fastor/ -I../Fastor/Fastor/ -I../eigen/ -c test.cpp -o $(OBJECTS)\test.o
+$(OBJECTS)/test.o: test.cpp
+	g++ -I../eigen/ -IC:/Users/zycru/OneDrive/Documents/c++/Fastor-0.6.4/Fastor -IC:/Users/zycru/OneDrive/Documents/c++/Fastor-0.6.4/ -c test.cpp -o $(OBJECTS)/test.o
 
 run:
-	.\tests\builds\test.exe
+	./tests/builds/test.exe
 
 clean:
-	del $(OBJECTS)\*.o .\tests\builds\test.exe
+	del $(OBJECTS)/*.o ./tests/builds/test.exe
 
 check-dir:
 	if not exist $(OBJECTS) mkdir objects
-	if not exist tests\builds mkdir tests\builds
+	if not exist tests/builds mkdir tests/builds
--- test.cpp
+++ test.cpp
@@ -6,128 +6,119 @@
 #include <random>
 
 using namespace std;
-// using namespace Eigen;
+using namespace Fastor;
 
-// MatrixXd Mask(int size, float lower=0, float upper=-numeric_limits<float>::infinity());
 
-// MatrixXd Softmax(const MatrixXd &x) {
-//     MatrixXd exp_x = x.array().exp();
-//     return exp_x.array().colwise() / exp_x.rowwise().sum().array();
-// }
+template<size_t x , size_t y>
+Tensor<float, x, y> RandomMatrix() {
+    // Define the mean and standard deviation
+    double mean = 0.0;
+    double stddev = 1.0;
 
-// MatrixXd ScaledDotProductAttention(const MatrixXd &Q, const MatrixXd &K, const MatrixXd &V, double scale_factor, bool isMasked=false) {
-//     MatrixXd scores = (Q * K.transpose()) / std::sqrt(scale_factor);
-//     if (isMasked) scores += Mask(scores.rows());
-//     MatrixXd attention_weights = Softmax(scores);
+    // Create a random device and seed the generator
+    random_device rd;
+    mt19937 gen(rd());
 
-//     // //Display
-//     // if (isMasked) {
-//     //     cout << "Mask: \n";
-//     //     cout << Mask(attention_weights.rows()) << "\n\n";
-//     // }
-//     // cout << "Softmax: \n";
-//     // cout << attention_weights << "\n\n";
+    // Create a normal distribution with the specified mean and standard deviation
+    normal_distribution<> d(mean, stddev);
 
-//     return attention_weights * V;
-// }
+    Tensor<float, x, y> random_matrix;
 
-// MatrixXd RandomMatrix(int rows, int cols) {
-//     // Initialize random number generator with a random seed
-//     random_device rd;
-//     mt19937 gen(rd());
-    
-//     // Standard normal distribution (mean = 0, stddev = 1)
-//     normal_distribution<> dist(0.0, 1.0);
-    
-//     // Create a matrix of given size
-//     MatrixXd matrix(rows, cols);
-    
-//     // Fill the matrix with random numbers
-//     for (int i = 0; i < rows; ++i) {
-//         for (int j = 0; j < cols; ++j) {
-//             matrix(i, j) = dist(gen);  // Sample from the standard normal distribution
-//         }
-//     }
-    
-//     return matrix;
-// }
-
-// MatrixXd Mask(int size, float lower, float upper) {
-//     MatrixXd res = MatrixXd(size, size);
-//     for (int i = 0; i < size; i++){
-//         for (int j = 0; j < size; j++){
-//             if (i >= j) res(i, j) = lower;
-//             else res(i, j) = upper;
-//         }
-//     }
-//     return res;
-// }
-
-// void test() {
-//     cout << "test" << endl;
-
-//     int l = 4;
-//     int dv = 8;
-//     int dk = 8;
-//     MatrixXd q = RandomMatrix(l, dk);
-//     MatrixXd k = RandomMatrix(l, dk);
-//     MatrixXd v = RandomMatrix(l, dv);
-
-//     cout << "Q\n";
-//     cout << q << "\n\n";
-//     cout << "K\n";
-//     cout << k << "\n\n";
-//     cout << "V\n";
-//     cout << v << "\n\n";
-
-//     cout << "Scaled Attention\n";
-//     MatrixXd attention = ScaledDotProductAttention(q, k, v, dk);
-
-//     cout << attention << "\n\n";
-
-//     cout << "[MASKED] Scaled Attention\n";
-//     MatrixXd attentionMasked = ScaledDotProductAttention(q, k, v, dk, true);
-
-//     cout << attentionMasked << "\n\n";
-// }
-
-template<size_t x, size_t y, size_t z>
-Fastor::Tensor<double, x, y, z> RandomMatrix() {
-    // Initialize random number generator with a random seed
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    
-    // Standard normal distribution (mean = 0, stddev = 1)
-    std::normal_distribution<> dist(0.0, 1.0);
-    
-    // Create a Fastor tensor of the given size
-    Fastor::Tensor<double, x, y, z> tensor;
-    
-    // Fill the tensor with random numbers
-    for (size_t i = 0; i < x; i++) {
-        for (size_t j = 0; j < y; j++) {
-            for (size_t k = 0; k < z; k++) {
-                tensor(i, j, k) = dist(gen);  // Sample from the standard normal distribution
-            }
+    // Genrate and insert random value from the normal distribution to the matrix
+    for (int i = 0; i < x; i++){
+        for (int j = 0; j < y; j++){
+            random_matrix(i, j) = d(gen);
         }
     }
-    
-    return tensor;
+
+    return random_matrix;
 }
 
-void test2() {
-    const int sequence_length = 4;
-    const int batch_size = 1;
-    const int input_dim = 512;
-    int d_model = 512;
-    auto x = RandomMatrix<batch_size, sequence_length, input_dim>();
-
-
+template<size_t x, size_t y>
+Tensor<float, x, y> Mask(float lower=0, float upper=-numeric_limits<float>::infinity()) {
+    Tensor<float, x, y> mask;
+    for (int i = 0; i < x; i++){
+        for (int j = 0; j < y; j++){
+            if (i < j) mask(i, j) = upper;
+            else mask(i, j) = lower;
+        }
+    }
+    return mask;
 }
+
+/*
+    Softmax
+*/
+template<size_t x, size_t y>
+Tensor<float, x, y> Softmax(const Tensor<float, x, y>& scaled) {
+    Tensor<float, x, y> expScaled = exp(scaled);
+    Tensor<float, x, y> sumExpScaled = sum(expScaled);
+    Tensor<float, x, y> softmax = expScaled/sumExpScaled;
+
+    return softmax;
+}
+
+/*
+    Single-Headed Attention
+*/
+template<size_t L, size_t dk, size_t dv>
+Tensor<float, L, dv> SingleHeadAttention(const Tensor<float, L, dk>& q,const Tensor<float, L, dk>& k, const Tensor<float, L, dv>& v, bool isMasked=false, bool display=false) {
+    // Scaled
+    Tensor<float, L, L> scaled = matmul(q, transpose(k)) / sqrt(dk);
+    if (isMasked) scaled += Mask<L, L>();
+
+    // Softmax
+    Tensor<float, L, L> softmax = Softmax<L, L>(scaled);
+
+    // Attention
+    Tensor<float, L, dv> attention = matmul(softmax, v);
+
+    if (display) {
+        cout << "Q:\n";
+        cout << q << "\n\n";
+
+        cout << "K:\n";
+        cout << k << "\n\n";
+
+        cout << "V:\n";
+        cout << v << "\n\n";
+
+        cout << "Softmax\n";
+        cout << softmax << "\n\n";
+
+        cout << "Single-headed Attention:\n";
+        cout << attention << "\n\n";
+    }
+
+    return attention;
+}
+
+
+void test1() {
+    const int L = 4; const int dk = 8; const int dv = 8;
+    Tensor<float, L, dk> q = RandomMatrix<L, dk>();
+    Tensor<float, L, dk> k = RandomMatrix<L, dk>();
+    Tensor<float, L, dv> v = RandomMatrix<L, dv>();
+
+    //Single-headed Attention
+    auto attention = SingleHeadAttention<L, dk, dv>(q, k, v, false, true);
+}
+
+
+// void test2() {
+//     const int sequence_length = 4;
+//     const int batch_size = 1;
+//     const int input_dim = 512;
+//     int d_model = 512;
+//     auto t = RandomMatrix<5, 5>();
+//     auto mask = Mask<4, 4>();
+
+//     cout << mask << endl;
+// }
 
 
 int main(){
-    test2();
+    test1();
 
     return 0;
 }
