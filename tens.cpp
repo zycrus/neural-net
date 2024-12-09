1c073f539eba13a7c90dd0a81d2a821cdc6cbef1
zycrus <phantom.bot.202@gmail.com>
2024-12-09 22:11:19 +0800

    Added operator for tensor multiplication

--- tens.cpp
+++ tens.cpp
@@ -9,13 +9,36 @@
 
 
 void run() {
-	Tensor x({1, 10});
-	// Tensor y({3, 10});
+	Tensor x({2, 5});
+	Tensor y({4, 10});
 	x.generate_random();
-	// y.generate_random();
-	// x.print();
-	// y.print();
-	// (x + y).print();
+	y.generate_random();
+	x.print();
+	y.print();
+	// x.matmul(x, y).print();
+
+
+    // Tensor new_tensor = x * y;
+
+    // std::cout << x.get_shape().size() << " " << y.get_shape().size() << std::endl;
+    // for (size_t x : new_tensor.get_shape()) {
+    //     std::cout << x << " ";
+    // }
+    // std::cout << std::endl;
+
+    // new_tensor.fill(0);
+
+    // new_tensor.print();
+    
+    // for (size_t i = 0; i < x.get_size(x.get_shape() ); i++) {
+    //     std::vector<size_t> new_idx = x.get_indices(i);
+    //     for (size_t x : new_idx) {
+    //         std::cout << x << " ";
+    //     }   
+    //     std::cout << std::endl;
+    // }
+
+    (x * y).print();
 }
 
 int main() {
@@ -75,9 +98,42 @@ const std::vector<size_t> Tensor::get_shape() const {
 	return shape;
 }
 
+// Convert flat index to tensor indices
+std::vector<size_t> Tensor::get_indices(const size_t flat_index) const {
+    std::vector<size_t> result(shape.size());
+    size_t temp = flat_index;
+
+    for (int i = shape.size() - 1; i >= 0; i--) {
+        result[i] = temp % shape[i];
+        temp = flat_index / shape[i];
+    }
+    return result;
+}
+
+
+// Getter for row
+const std::vector<T> Tensor::get_row(size_t row) const {
+    assert (this->shape.size() == 2);
+    std::vector<T> result(this->shape[1]);
+    for (size_t i = 0; i < this->shape[1]; i++) {
+        result[i] = (*this)({row, i});
+    }
+    return result;
+}
+
+// Getter for column
+const std::vector<T> Tensor::get_column(size_t col) const {
+    assert (this->shape.size() == 2);
+    std::vector<T> result(this->shape[0]);
+    for (size_t i = 0; i < this->shape[0]; i++) {
+        result[i] = (*this)({i, col});
+    }
+    return result;
+}
+
  // Fill tensor with a specific value
 void Tensor::fill(const T& value) {
-    std::fill(data.begin(), data.end(), value);
+    std::fill(this->data.begin(), this->data.end(), value);
 }
 
 void Tensor::generate_random() {
@@ -86,24 +142,73 @@ void Tensor::generate_random() {
 
     // Genrate and insert random value from the normal distribution to the matrix
     for (int i = 0; i < data.size(); i++) {
-            data[i] = d(gen);
+            this->data[i] = d(gen);
     }
 }
 
 
+// OPERATORS
 
-// Add operator
+// Add Tensors(element-wise)
 Tensor Tensor::operator+(const Tensor other) const{	
     // Check if shapes match
-    assert (shape == other.get_shape());
+    assert (this->shape == other.get_shape());
 
     // Create a new tensor to hold the result
-    Tensor result(shape);
+    Tensor result(this->shape);
 
     // Perform element-wise addition
     for (size_t i = 0; i < total_size; ++i) {
-        result.data[i] = data[i] + other.data[i];
+        result.data[i] = this->data[i] + other.data[i];
     }
+
+    return result;
+}
+
+// Multiply Scalar to Tensor
+Tensor Tensor::operator*(const T other) const {
+    Tensor result(this->shape);
+
+    for (size_t i = 0; i < get_size(this->shape); i++) {
+        std::vector<size_t> idx = result.get_indices(i);
+        result(idx) = (*this)(idx) * other;
+    }
+
+    return result;
+}
+
+
+
+// Multiply Tensors
+Tensor Tensor::operator*(const Tensor other) const {
+    std::vector<size_t> result_shape;
+    // Concatenate both tensor shape to a new vector
+    for (size_t dim : this->shape) {
+        result_shape.insert(result_shape.end(), dim);
+    }
+    for (size_t dim : other.get_shape()) {
+        result_shape.insert(result_shape.end(), dim);
+    }
+
+    // Create new Tensor object
+    Tensor result(result_shape);
+    std::vector<size_t> indices(result_shape.size());
+
+    
+    for (size_t i = 0; i < get_size(this->shape); i++) {
+        std::vector<size_t> idx_outer = get_indices(i);
+        for (size_t j = 0; j < get_size(other.get_shape()); j++) {
+            std::vector<size_t> idx;
+            idx = idx_outer;
+            std::vector<size_t> idx_inner = other.get_indices(j);
+            for (size_t dim : idx_inner) {
+                idx.insert(idx.end(), dim);
+            }
+
+            result(idx) = (*this)(idx_outer) * other(idx_inner);
+        }
+    }
+
 
     return result;
 }
@@ -111,19 +216,45 @@ Tensor Tensor::operator+(const Tensor other) const{
 // Access operator (for both getting and setting values)
 T& Tensor::operator()(const std::vector<size_t>& indices) {
     size_t flat_idx = get_flat_index(indices);
-    return data[flat_idx];
+    return this->data[flat_idx];
 }
 
 // Const version of the access operator (for getting values)
 const T& Tensor::operator()(const std::vector<size_t>& indices) const {
     size_t flat_idx = get_flat_index(indices);
-    return data[flat_idx];
+    return this->data[flat_idx];
+}
+
+// Vector Dot Product
+const T Tensor::vecdot(const std::vector<T> t1, const std::vector<T> t2) const {
+    T result = 0;
+    for (size_t i = 0; i < t1.size(); i++) {
+        result += t1[i] * t2[i];
+    }
+    return result;
+}
+
+// Multiply Matrix
+const Tensor Tensor::matmul(const Tensor t1, const Tensor t2, size_t depth1, size_t depth2) const {
+    assert (t1.get_shape().size() == 2 && t2.get_shape().size() == 2);
+    assert (t1.get_shape()[1] == t2.get_shape()[0]);
+
+    Tensor result({t1.get_shape()[1], t2.get_shape()[0]});
+
+    for (size_t i = 0; i < t1.get_shape()[1]; i++) {
+        for (size_t j = 0; j < t2.get_shape()[0]; j++) {
+            result({i, j}) = vecdot(t1.get_column(i), t2.get_row(j));
+        }
+    }
+
+    return result;
+
 }
 
 void Tensor::print(const std::vector<size_t>& indices, size_t depth) const {
-    if (depth == shape.size() - 1) {
+    if (depth == this->shape.size() - 1) {
         std::cout << "[ ";
-        for (size_t i = 0; i < shape[depth]; ++i) {
+        for (size_t i = 0; i < this->shape[depth]; ++i) {
             auto current_indices = indices;
             current_indices.push_back(i);
             if ((*this)(current_indices) > 0) std::cout << " ";
@@ -132,11 +263,11 @@ void Tensor::print(const std::vector<size_t>& indices, size_t depth) const {
         std::cout << "]";
     } else {
         std::cout << "[ ";
-        for (size_t i = 0; i < shape[depth]; ++i) {
+        for (size_t i = 0; i < this->shape[depth]; ++i) {
             auto current_indices = indices;
             current_indices.push_back(i);
             print(current_indices, depth + 1);  // Recursively call print()
-            if (i < shape[depth] - 1) {
+            if (i < this->shape[depth] - 1) {
                 std::cout << "\n";  // Add newline between matrix rows
                 for (size_t s = 0; s <= depth; s++) {
                 	std::cout << "  ";
--- tens.hpp
+++ tens.hpp
@@ -9,14 +9,11 @@
 class Tensor {
 private:
 	std::vector<T> data;     // Flat Data Storage
-    std::vector<size_t> shape;  // Tensor Dimensions
-    size_t total_size;  // Total number of elements in the Tensor
+   std::vector<size_t> shape;  // Tensor Dimensions
+   size_t total_size;  // Total number of elements in the Tensor
 
-    // Helper function to calculate total size
-    size_t get_size(const std::vector<size_t>& dims) const;
-
-    // Helper function to compute flat index from multi-dimensional indices
-    size_t get_flat_index(const std::vector<size_t>& indices) const;
+	// Helper function to compute flat index from multi-dimensional indices
+	size_t get_flat_index(const std::vector<size_t>& indices) const;
 
  public:
     // Constructor with shape
@@ -24,6 +21,12 @@ private:
 
     // Constructor with data and shape
 	Tensor(const std::vector<T>& data, const std::vector<size_t>& dims);
+
+   // Helper function to calculate total size
+   size_t get_size(const std::vector<size_t>& dims) const;
+
+   // Convert flat index to tensor indices
+   std::vector<size_t> get_indices(const size_t flat_index) const;
 
 	// Getter for shape
 	const std::vector<size_t> get_shape() const;
@@ -34,11 +37,29 @@ private:
 	// Generate Random Tensor values
 	void generate_random();
 
+	// Get Column
+	const std::vector<T> get_row(size_t row) const;
+
+	// Get Column
+	const std::vector<T> get_column(size_t col) const;
+
 	// Print Tensor
 	void print(const std::vector<size_t>& indices = {}, size_t depth = 0) const;
 
+	// Vector Dot Product
+	const T vecdot(const std::vector<T> t1, const std::vector<T> t2) const;
+
+	// Multiply Matrix
+	const Tensor matmul(const Tensor t1, const Tensor t2, size_t depth1 = 0, size_t depth2 = 0) const;
+
 	// Add operator
 	Tensor operator+(const Tensor other) const;
+
+	// Multiply Scalar to Tensor
+	Tensor operator*(const T other) const;
+
+	// Multiply Tensor
+	Tensor operator*(const Tensor other) const;
 
 	// Access operator (for both getting and setting values)
     T& operator()(const std::vector<size_t>& indices);
